f8973eba65b4d19c585e579e0fbdfd37ef57d952
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-02-14 21:39:10 +0100

    Add home.js

---
+++ data/home.js
@@ -0,0 +1,26 @@
+<div>
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/buns">Buns</a>
+      </li>
+    </ul>
+  </nav>
+  <div>
+    <p>
+      Swedish fika bread is an essential part of Sweden's beloved coffee break
+      tradition, known as *fika*. These sweet, spiced, and often buttery
+      pastries accompany coffee and conversation, creating a cozy social
+      experience. Popular varieties include *kanelbullar* (cinnamon buns),
+      *kardemummabullar* (cardamom buns), and *vetebröd* (sweet wheat bread).
+      Made with enriched dough, often flavored with cardamom, cinnamon, or
+      vanilla, these treats are soft, aromatic, and slightly sticky from sugar
+      glazes or pearl sugar toppings. Fika bread is more than just a snack; it
+      represents warmth, hospitality, and the Swedish love for slowing down and
+      enjoying life's small pleasures.
+    </p>
+  </div>
+</div>
--- index.js
+++ index.js
@@ -1,15 +1,33 @@
+import fs from "fs";
 import http from "http";
+import url from "url";
 
 const PORT = 3007;
 
-const server = http
+http
   .createServer((req, res) => {
-    const path = url.parse(req.url, true);
+    const fullpath = url.parse(req.url, true);
+    let queries = fullpath.query;
 
-
+    if (fullpath.path === "/") {
+      res.write("<h1>Welcome to Bread and Butter</h1>");
+      fs.readFile("./data/home.js", (err, data) => {
+        if (err) {
+          res.write("<p>Something went wrong</p>");
+        } else {
+          res.write(data);
+        }
+      });
+    } else if (fullpath.path === "/buns") {
+      fs.readFile("./data/navMain.js", (err, data) => {
+        if (err) {
+          res.write("<p>Something went wrong</p>");
+        } else {
+          res.write(data);
+        }
+      });
+    }
   })
   .listen(PORT, () =>
-    console.log(`🥐 Connecting on port ${PORT} ➡️ http://localhost:3001`)
+    console.log(`🥐 Connecting on port ${PORT} ➡️ http://localhost:${PORT}`)
   );
-
-server();
